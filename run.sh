751585d0f3b39d5ef51ae7c5155ed98d64970483
vekaomg <146497829+vekalmao@users.noreply.github.com>
2024-06-04 16:03:14 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
 DIR=$PWD # get current dir
 # if current dir is /root print ~
-if [[ "$PWD" == /root* ]]; then
+if [ "$PWD" = "/root"* ]; then
     DIR="~${PWD#/root}"
 fi
 
-clear
+printf "\033c"
 printf "${GREEN}╭────────────────────────────────────────────────────────────────────────────────╮${NC}\n"
 printf "${GREEN}│                                                                                │${NC}\n"
 printf "${GREEN}│                             LylaNodes VM - EGG                                │${NC}\n"
@@ -17,22 +17,35 @@ printf "${GREEN}│                                                             
 printf "${GREEN}│                           ${RED}© 2021 - 2024 vizle${GREEN}                               │${NC}\n"
 printf "${GREEN}│                                                                                │${NC}\n"
 printf "${GREEN}╰────────────────────────────────────────────────────────────────────────────────╯${NC}\n"
-printf "\n"
-printf "root@LylaNodesVM:${DIR}# \n"
+printf "                                                                                               \n"
+printf "root@LylaNodesVM:${DIR}#                                                                             \n"
 
 run_cmd() {
-    while true; do
-        read -e -p "root@LylaNodesVM:$DIR# " CMD
-        bash -c "$CMD"  # Directly use bash to run the command
+    read -p "root@LylaNodesVM:$DIR# " CMD
+    eval "$CMD"
 
-        # Update DIR after executing command
-        DIR=$PWD
-        if [[ "$PWD" == /root* ]]; then
-            DIR="~${PWD#/root}"
-        fi
-        
-        printf "root@LylaNodesVM:$DIR# \n"
-    done
+    # Update DIR after executing command
+    DIR=$PWD
+    if [ "$PWD" = "/root"* ]; then
+        DIR="~${PWD#/root}"
+    fi
+    
+    printf "root@LylaNodesVM:$DIR# \n"
+    run_user_cmd
+}
+
+run_user_cmd() {
+    read -p "user@LylaNodesVM:$DIR# " CMD2
+    eval "$CMD2"
+
+    # Update DIR after executing command
+    DIR=$PWD
+    if [ "$PWD" = "/root"* ]; then
+        DIR="~${PWD#/root}"
+    fi
+    
+    printf "root@LylaNodesVM:$DIR# \n"
+    run_cmd
 }
 
 run_cmd

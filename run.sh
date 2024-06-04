1c0bb12928194eed64fca0857d6b57af5db9ff6e
vekaomg <146497829+vekalmao@users.noreply.github.com>
2024-06-04 15:48:21 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 
 DIR=$PWD # get current dir
 # if current dir is /root print ~
-if [ "$PWD" = "/root"* ]; then
+if [[ "$PWD" == /root* ]]; then
     DIR="~${PWD#/root}"
 fi
 
@@ -21,31 +21,18 @@ printf "                                                                        
 printf "root@LylaNodesVM:${DIR}#                                                                             \n"
 
 run_cmd() {
-    read -p "root@LylaNodesVM:$DIR# " CMD
-    eval "$CMD"
+    while true; do
+        read -p "root@LylaNodesVM:$DIR# " CMD
+        eval "$CMD"
 
-    # Update DIR after executing command
-    DIR=$PWD
-    if [ "$PWD" = "/root"* ]; then
-        DIR="~${PWD#/root}"
-    fi
-    
-    printf "root@LylaNodesVM:$DIR# \n"
-    run_user_cmd
-}
-
-run_user_cmd() {
-    read -p "user@LylaNodesVM:$DIR# " CMD2
-    eval "$CMD2"
-
-    # Update DIR after executing command
-    DIR=$PWD
-    if [ "$PWD" = "/root"* ]; then
-        DIR="~${PWD#/root}"
-    fi
-    
-    printf "root@LylaNodesVM:$DIR# \n"
-    run_cmd
+        # Update DIR after executing command
+        DIR=$PWD
+        if [[ "$PWD" == /root* ]]; then
+            DIR="~${PWD#/root}"
+        fi
+        
+        printf "root@LylaNodesVM:$DIR# \n"
+    done
 }
 
 run_cmd
